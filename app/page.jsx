0f327ffda612ef7f6ff094697ed623a2f5655b2b
FaridRuano <fruanocm2777@gmail.com>
2024-04-02 22:12:43 -0500

    Third Section

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image"
 import AllpaLogo from "@public/assets/main-logo.png"
 import AllpaLogo2 from "@public/assets/main-logo2.png"
+import MainBg from "@public/images/main-bg.png"
+import SmAllpaLogo from "@public/assets/sm-logo-sec.png"
 
 
 export default function Home() {
@@ -9,10 +11,30 @@ export default function Home() {
       <section className="first-sec">
         <div className="gradient-overlay"/>
         <div className="logo-warp">
-          <Image className="img" src={AllpaLogo} width={545} height={180} alt="Allpa"/>
-          <Image className="img" src={AllpaLogo2} width={544} height={46} alt="Allpa"/>
+          <Image src={AllpaLogo} width={290} height={'auto'} alt="Allpa"/>
+          <Image src={AllpaLogo2} width={290} height={'auto'} alt="Allpa"/>
         </div>
-      </section>      
+        <Image className="main-bg" src={MainBg} width={'auto'} height={810} alt="Allpa Render"/>
+      </section>     
+      <section className="second-sec">
+        <Image className="sm-logo" src={SmAllpaLogo} width={120} height={'auto'} alt="Allpa"/>
+        <p>
+          <b>Descubre una nueva forma de vivir en Guápulo,</b> donde la belleza natural se une con la
+          comodidad urbana. Allpa te invita a ser parte de una comunidad única que valora la
+          sustentabilidad y la conexion con la naturaleza.
+        </p>
+      </section>
+      <section className="third-sec">
+        <div className="txt-warp center">
+          <p className="active">Una vista<br/> que te <br/>da calma.</p>
+          <p>Naturaleza,<br/> Silencio <br/> & Cultura.</p>
+          <p>Un refugio<br/> para tu<br/> familia.</p>
+
+        </div>
+        <div className="img-warp">
+
+        </div>
+      </section>
     </main>
   )
 }
