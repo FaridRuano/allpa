708ec8c36ef916f08a098ec9276a86a4adeedd2f
FaridRuano <fruanocm2777@gmail.com>
2024-04-12 16:56:28 -0500

    Update Preview

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
-import { Menu } from "react-feather";
 import "./globals.scss";
+import { Head } from "next/document";
 
 export const metadata = {
   title: "Allpa",
@@ -9,6 +9,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta property="og:title" content="Allpa" />
+          <meta property="og:description" content="habitar.compartir" />
+          <meta property="og:image" content="/public/images/main-bg.png" />
+          <meta property="og:url" content="allpahabitar.com" />
+          <meta property="og:type" content="website" />
+          <title>Título de tu página</title>
+        </Head>
       <body>
         <div className="app">
           {children}
