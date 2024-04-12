f0260310be5997e740fd042e8fcef33752d9ee60
FaridRuano <fruanocm2777@gmail.com>
2024-04-12 17:12:54 -0500

    Bug Fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,4 @@
 import "./globals.scss";
-import { Head } from "next/document";
 import MainBg2 from "@public/images/main-bg2.png"
 
 
@@ -11,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
           <meta charset="UTF-8" />
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -21,7 +20,7 @@ export default function RootLayout({ children }) {
           <meta property="og:url" content="allpahabitar.com" />
           <meta property="og:type" content="website" />
           <title>Título de tu página</title>
-        </Head>
+        </head>
       <body>
         <div className="app">
           {children}
