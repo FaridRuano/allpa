8b27683a3f684162a4e3a909bb14fc38da5ec2d6
FaridRuano <fruanocm2777@gmail.com>
2024-04-16 21:47:39 -0500

    Update Metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,11 +13,6 @@ export default function RootLayout({ children }) {
           <meta charset="UTF-8" />
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta property="og:title" content="Allpa" />
-          <meta property="og:description" content="habitar.compartir" />
-          <meta property="og:image" content="./opengraph-image.jpg" />
-          <meta property="og:url" content="allpahabitar.com" />
-          <meta property="og:type" content="website" />
           <title>Título de tu página</title>
         </head>
       <body>
